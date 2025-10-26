2a11d777e13acbc5afb8db8b6a75c1d4adca6056
Gab27x <gabrielernestoeb@outlook.com>
2025-10-26 11:14:13 -0500

    fix: user service mappers

--- user-service/src/main/java/com/selimhorri/app/helper/AddressMappingHelper.java
+++ user-service/src/main/java/com/selimhorri/app/helper/AddressMappingHelper.java
@@ -7,52 +7,72 @@ import com.selimhorri.app.dto.UserDto;
 
 public interface AddressMappingHelper {
 	
-	public static AddressDto map(final Address address) {
+	static AddressDto map(final Address address) {
+		if (address == null) {
+			return null;
+		}
+		
+		UserDto userDto = null;
+		if (address.getUser() != null) {
+			userDto = UserDto.builder()
+					.userId(address.getUser().getUserId())
+					.firstName(address.getUser().getFirstName())
+					.lastName(address.getUser().getLastName())
+					.imageUrl(address.getUser().getImageUrl())
+					.email(address.getUser().getEmail())
+					.phone(address.getUser().getPhone())
+					.build();
+		}
+		
 		return AddressDto.builder()
 				.addressId(address.getAddressId())
 				.fullAddress(address.getFullAddress())
 				.postalCode(address.getPostalCode())
 				.city(address.getCity())
-				.userDto(
-					UserDto.builder()
-						.userId(address.getUser().getUserId())
-						.firstName(address.getUser().getFirstName())
-						.lastName(address.getUser().getLastName())
-						.imageUrl(address.getUser().getImageUrl())
-						.email(address.getUser().getEmail())
-						.phone(address.getUser().getPhone())
-						.build())
+				.userDto(userDto)
+				.build();
+	}
+	
+	// Método para evitar referencia circular al mapear desde User
+	static AddressDto mapWithoutUser(final Address address) {
+		if (address == null) {
+			return null;
+		}
+		
+		return AddressDto.builder()
+				.addressId(address.getAddressId())
+				.fullAddress(address.getFullAddress())
+				.postalCode(address.getPostalCode())
+				.city(address.getCity())
 				.build();
 	}
 	
 	public static Address map(final AddressDto addressDto) {
-		return Address.builder()
+		if (addressDto == null) {
+			return null;
+		}
+		
+		Address address = Address.builder()
 				.addressId(addressDto.getAddressId())
 				.fullAddress(addressDto.getFullAddress())
 				.postalCode(addressDto.getPostalCode())
 				.city(addressDto.getCity())
-				.user(
-					User.builder()
-						.userId(addressDto.getUserDto().getUserId())
-						.firstName(addressDto.getUserDto().getFirstName())
-						.lastName(addressDto.getUserDto().getLastName())
-						.imageUrl(addressDto.getUserDto().getImageUrl())
-						.email(addressDto.getUserDto().getEmail())
-						.phone(addressDto.getUserDto().getPhone())
-						.build())
 				.build();
+		
+		// Establecer relación bidireccional con User
+		if (addressDto.getUserDto() != null) {
+			User user = User.builder()
+					.userId(addressDto.getUserDto().getUserId())
+					.firstName(addressDto.getUserDto().getFirstName())
+					.lastName(addressDto.getUserDto().getLastName())
+					.imageUrl(addressDto.getUserDto().getImageUrl())
+					.email(addressDto.getUserDto().getEmail())
+					.phone(addressDto.getUserDto().getPhone())
+					.build();
+			address.setUser(user);
+		}
+		
+		return address;
 	}
 	
-	
-	
 }
-
-
-
-
-
-
-
-
-
-
